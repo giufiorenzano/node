6b9d18fd5ff12c613d505f9f998ce6378b16b903
giufiorenzano <giufiorenzano@gmail.com>
2022-03-23 18:11:35 -0300

    add some other concepts

---
+++ process.js
@@ -0,0 +1,5 @@
+console.log(process.argv)
+
+// escrevendo no terminal:
+// node process --name "Giulia" --greeting "Tudo bem?"
+// --name e --greetings sao chamadas de flag, e recebem os parametros passados logo após cada uma
